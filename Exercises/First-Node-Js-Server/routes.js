1bdace6734d7830dfee011e7b7cfe08bf55f9ceb
TheRealLeonZZ <140121089+TheRealLeonZZ@users.noreply.github.com>
2025-01-03 19:09:35 +0200

    Installed first npm module nodemon

--- Exercises/First-Node-Js-Server/routes.js
+++ Exercises/First-Node-Js-Server/routes.js
@@ -1,35 +1,38 @@
 const fs = require('fs');
 
 const requestHandler = (req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    // This will run when the request starts
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    // This will run when the request ends
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
+	const url = req.url;
+	const method = req.method;
+	if (url === '/') {
+		res.write('<html>');
+		res.write('<head><title>My First Page</title></head>');
+		res.write('<body>');
+		res.write('<h1>My First Page</h1>');
+		res.write(
+			"<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+		);
+		res.write('</body>');
+		res.write('</html>');
+		return res.end();
+	}
+	if (url === '/message' && method === 'POST') {
+		const body = [];
+		// This will run when the request starts
+		req.on('data', (chunk) => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+		// This will run when the request ends
+		return req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+			fs.writeFile('message.txt', message, (err) => {
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				return res.end();
+			});
+		});
+	}
 };
 
 // module.exports = requestHandler;
