90409ac995329061f9722331455192a1e14f1b41
TheRealLeonZZ <140121089+TheRealLeonZZ@users.noreply.github.com>
2025-01-03 18:11:23 +0200

    Splitting server into 2 files, one of just the server and one that handles the routes

--- Exercises/First-Node-Js-Server/app.js
+++ Exercises/First-Node-Js-Server/app.js
@@ -1,27 +1,9 @@
 // Add the http module
 const http = require('http');
-// Add the fs module
-const fs = require('fs');
+const routes = require('./routes');
 
 // Create a server that takes in a request and a response
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (req.url === '/message' && req.method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-});
+const server = http.createServer(routes);
 
 //Listen to port 3000
 server.listen(3000);
