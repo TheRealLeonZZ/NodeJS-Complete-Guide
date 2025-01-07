7b34c3fd71f8c89e832ce727c7b1d946fa198e9d
LeonSch <leoneli61@gmail.com>
2025-01-07 11:36:54 +0200

    updating express router for /admin/path

--- Exercises/First-Express-Js-Server/routes/admin.js
+++ Exercises/First-Express-Js-Server/routes/admin.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-router.use(bodyParser.urlencoded({ extended: false })); //parser for form data
-
 router.get('/add-product', (req, res, next) => {
 	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+		'<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
 	);
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
 	console.log(req.body); // { title: 'Test' }
 	res.redirect('/');
 });
