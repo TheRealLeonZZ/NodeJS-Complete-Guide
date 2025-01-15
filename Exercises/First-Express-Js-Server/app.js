a7f6baa172c3ffdb02a95a4ac1e00091f0fa9931
LeonSch <leoneli61@gmail.com>
2025-01-15 14:14:02 +0200

    Serving css files statically

--- Exercises/First-Express-Js-Server/app.js
+++ Exercises/First-Express-Js-Server/app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })); //parser for form data
+app.use(express.static(path.join(__dirname, 'public'))); //Grant read access to this folder
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
