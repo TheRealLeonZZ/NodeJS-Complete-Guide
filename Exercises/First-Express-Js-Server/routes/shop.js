d92b7f18e5ec4d3e3a2c5243f19fe98bdf0084e6
LeonSch <leoneli61@gmail.com>
2025-01-18 13:44:13 +0200

    Added dynamic active class to navbar

--- Exercises/First-Express-Js-Server/routes/shop.js
+++ Exercises/First-Express-Js-Server/routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	const products = adminData.products;
-	res.render('shop', { prods: products, docTitle: 'Shop' }); //Arguments are passing dymanic data
+	res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' }); //Arguments are passing dymanic data
 });
 
 module.exports = router;
