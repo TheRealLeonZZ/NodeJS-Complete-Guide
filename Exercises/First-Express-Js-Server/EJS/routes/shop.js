b7bcb53f5925509db53a7771e0ec14e9c87ebf6e
LeonSch <leoneli61@gmail.com>
2025-01-18 15:10:07 +0200

    Transformed to EJS

---
+++ Exercises/First-Express-Js-Server/EJS/routes/shop.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	const products = adminData.products;
+	res.render('shop', {
+		prods: products,
+		pageTitle: 'Shop',
+		path: '/',
+	}); //Arguments are passing dymanic data
+});
+
+module.exports = router;
