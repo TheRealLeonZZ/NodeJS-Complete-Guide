791bb02063bafcc7f9d50581a7545449863b26b6
LeonSch <leoneli61@gmail.com>
2025-01-18 15:15:05 +0200

    Updated files

--- Exercises/First-Express-Js-Server/Handlebars/routes/shop.js
+++ Exercises/First-Express-Js-Server/Handlebars/routes/shop.js
@@ -11,7 +11,6 @@ router.get('/', (req, res, next) => {
 	res.render('shop', {
 		prods: products,
 		pageTitle: 'Shop',
-		path: '/',
 		hasProducts: products.length > 0,
 		activeShop: true,
 		productCSS: true,
