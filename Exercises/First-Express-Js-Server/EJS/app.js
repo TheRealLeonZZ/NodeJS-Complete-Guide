7f2ff288bb50e2e62b463fefb8e8c35f95ef517e
LeonSch <leoneli61@gmail.com>
2025-01-20 12:32:05 +0200

    adding controllers

--- Exercises/First-Express-Js-Server/EJS/app.js
+++ Exercises/First-Express-Js-Server/EJS/app.js
@@ -3,22 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs'); //Which view engine to use
 app.set('views', 'views'); //Where to look for views
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })); //parser for form data
 app.use(express.static(path.join(__dirname, 'public'))); //Grant read access to this folder
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', { pageTitle: 'Page Not Found', path: req.url });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
