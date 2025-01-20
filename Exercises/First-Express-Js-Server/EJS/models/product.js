63f047e3219029066b564eb7327070371f67e59b
LeonSch <leoneli61@gmail.com>
2025-01-20 17:55:52 +0200

    Expand models and working with files

--- Exercises/First-Express-Js-Server/EJS/models/product.js
+++ Exercises/First-Express-Js-Server/EJS/models/product.js
@@ -1,4 +1,18 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, '..', 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			const products = JSON.parse(fileContent);
+			cb(products);
+		}
+	});
+};
 
 module.exports = class Product {
 	constructor(t) {
@@ -6,10 +20,15 @@ module.exports = class Product {
 	}
 
 	save() {
-		products.push(this);
+		getProductsFromFile((products) => {
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
 	}
 
-	static fetchAll() {
-		return products;
+	static fetchAll(cb) {
+		getProductsFromFile(cb);
 	}
 };
