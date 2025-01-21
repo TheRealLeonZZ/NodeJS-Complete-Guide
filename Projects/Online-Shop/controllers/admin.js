cbaa896dec97f7cd929b59c70b3450d9feeeaa77
LeonSch <leoneli61@gmail.com>
2025-01-21 16:51:32 +0200

    Refactoring product model

--- Projects/Online-Shop/controllers/admin.js
+++ Projects/Online-Shop/controllers/admin.js
@@ -18,7 +18,11 @@ exports.getAdminProducts = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-	const product = new Product(req.body.title);
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const description = req.body.description;
+	const price = req.body.price;
+	const product = new Product(title, imageUrl, description, price);
 	product.save();
 	res.redirect('/');
 };
