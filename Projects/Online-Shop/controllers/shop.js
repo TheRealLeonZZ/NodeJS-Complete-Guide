185ff4ad78fd321c539c61b2a759fe2d184395cd
Leon Sch <leoneli61@gmail.com>
2025-01-25 15:18:51 +0200

    Creating Cart model

--- Projects/Online-Shop/controllers/shop.js
+++ Projects/Online-Shop/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
 	Product.fetchAll((products) => {
@@ -35,6 +36,14 @@ exports.getCart = (req, res, next) => {
 	res.render('shop/cart', {
 		pageTitle: 'Your Cart',
 		path: '/cart',
+	});
+};
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.addProduct(prodId, product.price);
+		res.redirect('/cart');
 	});
 };
 
