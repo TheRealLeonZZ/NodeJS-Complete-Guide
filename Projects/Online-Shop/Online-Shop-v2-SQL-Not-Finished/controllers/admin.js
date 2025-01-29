45601d735eb2a651409cbcf8209cdb8f3dba773a
LeonSch <leoneli61@gmail.com>
2025-01-29 14:45:35 +0200

    Updating the edited product with sequelizer

--- Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
@@ -52,15 +52,19 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedDescription = req.body.description;
 	const updatedPrice = req.body.price;
-	const updatedProduct = new Product(
-		prodId,
-		updatedTitle,
-		updatedImageUrl,
-		updatedDescription,
-		updatedPrice
-	);
-	updatedProduct.save();
-	res.redirect('/admin/products');
+	Product.findByPk(prodId)
+		.then((product) => {
+			product.title = updatedTitle;
+			product.imageUrl = updatedImageUrl;
+			product.description = updatedDescription;
+			product.price = updatedPrice;
+			return product.save();
+		})
+		.then((result) => {
+			console.log('UPDATED PRODUCT');
+			res.redirect('/admin/products');
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
