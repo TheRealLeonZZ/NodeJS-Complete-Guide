68a106dde2679732bebcb722a19885d824ec0b42
LeonSch <leoneli61@gmail.com>
2025-01-29 14:37:53 +0200

    Getting admin products with sequelizer

--- Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
@@ -68,11 +68,13 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getAdminProducts = (req, res, next) => {
-	Product.fetchAll((products) => {
-		res.render('admin/products', {
-			prods: products,
-			pageTitle: 'Admin Products',
-			path: '/admin/products',
-		}); //Arguments are passing dynamic data
-	});
+	Product.findAll()
+		.then((products) => {
+			res.render('admin/products', {
+				prods: products,
+				pageTitle: 'Admin Products',
+				path: '/admin/products',
+			}); //Arguments are passing dynamic data
+		})
+		.catch((err) => console.log(err));
 };
