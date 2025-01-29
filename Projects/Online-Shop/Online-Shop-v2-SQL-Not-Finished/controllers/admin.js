7ff6b3b65014590b9465ee72ae09c807e3822c1f
LeonSch <leoneli61@gmail.com>
2025-01-29 14:40:25 +0200

    Get edit products with sequelizer

--- Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
@@ -31,17 +31,19 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect('/');
 	}
 	const prodId = req.params.productId;
-	Product.findById(prodId, (product) => {
-		if (!product) {
-			return res.redirect('/');
-		}
-		res.render('admin/edit-product', {
-			pageTitle: 'Edit Product',
-			path: '/admin/edit-product',
-			editing: true,
-			product: product,
-		});
-	});
+	Product.findByPk(prodId)
+		.then((product) => {
+			if (!product) {
+				return res.redirect('/');
+			}
+			res.render('admin/edit-product', {
+				pageTitle: 'Edit Product',
+				path: '/admin/edit-product',
+				editing: true,
+				product: product,
+			});
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
