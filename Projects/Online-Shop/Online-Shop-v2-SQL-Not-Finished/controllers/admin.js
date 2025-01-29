d664f759e45f562eabf90959aec3507992cb7e47
LeonSch <leoneli61@gmail.com>
2025-01-29 14:52:21 +0200

    Deleting products using sequelizer

--- Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/admin.js
@@ -19,8 +19,9 @@ exports.postAddProduct = (req, res, next) => {
 		imageUrl: imageUrl,
 		description: description,
 	})
-		.then(() => {
-			res.redirect('/');
+		.then((result) => {
+			console.log('CREATED PRODUCT');
+			res.redirect('/admin/products');
 		})
 		.catch((err) => console.log(err));
 };
@@ -69,8 +70,15 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.deleteById(prodId);
-	res.redirect('/admin/products');
+	Product.findByPk(prodId)
+		.then((product) => {
+			return product.destroy();
+		})
+		.then((result) => {
+			console.log('DELETED PRODUCT');
+			res.redirect('/admin/products');
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.getAdminProducts = (req, res, next) => {
