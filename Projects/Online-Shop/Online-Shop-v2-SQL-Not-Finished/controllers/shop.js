0be701fa3bf4edba94d24b58381d8afdf8a51cf4
Leon Sch <leoneli61@gmail.com>
2025-02-05 23:13:21 +0200

    changed getCart function

--- Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/shop.js
+++ Projects/Online-Shop/Online-Shop-v2-SQL-Not-Finished/controllers/shop.js
@@ -39,27 +39,21 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-	Cart.getCart((cart) => {
-		Product.fetchAll((products) => {
-			const cartProducts = [];
-			for (let product of products) {
-				const cartProductData = cart.products.find(
-					(prod) => prod.id === product.id
-				);
-				if (cartProductData) {
-					cartProducts.push({
-						productData: product,
-						qty: cartProductData.qty,
+	req.user
+		.getCart()
+		.then((cart) => {
+			return cart
+				.getProducts()
+				.then((products) => {
+					res.render('shop/cart', {
+						pageTitle: 'Your Cart',
+						path: '/cart',
+						products: cartProducts,
 					});
-				}
-			}
-			res.render('shop/cart', {
-				pageTitle: 'Your Cart',
-				path: '/cart',
-				products: cartProducts,
-			});
-		});
-	});
+				})
+				.catch((err) => console.log(err));
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
