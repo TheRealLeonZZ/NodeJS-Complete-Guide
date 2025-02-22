85d186da59e150d77224226268826f0ed2f65624
Leon Sch <leoneli61@gmail.com>
2025-02-23 00:16:09 +0200

    Created basic login page

--- Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/app.js
+++ Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/app.js
@@ -15,6 +15,7 @@ app.set('views', 'views'); //Where to look for views
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false })); //parser for form data
 app.use(express.static(path.join(__dirname, 'public'))); //Grant read access to this folder
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
