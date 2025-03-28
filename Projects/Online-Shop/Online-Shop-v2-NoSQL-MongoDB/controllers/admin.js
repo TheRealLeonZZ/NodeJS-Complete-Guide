50c68c053df8a75c63476f9f02eb4760f704e114
LeonSch <leoneli61@gmail.com>
2025-03-28 18:41:57 +0300

    Adding validation for editing a product

--- Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/controllers/admin.js
@@ -23,6 +23,7 @@ exports.getAddProduct = (req, res, next) => {
 		editing: false,
 		hasError: false,
 		errorMessage: null,
+		validationErrors: [],
 	});
 };
 
@@ -45,6 +46,7 @@ exports.postAddProduct = (req, res, next) => {
 				description: description,
 			},
 			errorMessage: errors.array()[0].msg,
+			validationErrors: errors.array(),
 		});
 	}
 	const product = new Product({
@@ -81,6 +83,7 @@ exports.getEditProduct = (req, res, next) => {
 				product: product,
 				hasError: false,
 				errorMessage: null,
+				validationErrors: [],
 			});
 		})
 		.catch((err) => console.log(err));
@@ -92,6 +95,24 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedDescription = req.body.description;
 	const updatedImageUrl = req.body.imageUrl;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).render('admin/edit-product', {
+			pageTitle: 'Edit Product',
+			path: '/admin/edit-product',
+			editing: true,
+			hasError: true,
+			product: {
+				title: updatedTitle,
+				imageUrl: updatedImageUrl,
+				price: updatedPrice,
+				description: updatedDescription,
+				_id: prodId,
+			},
+			errorMessage: errors.array()[0].msg,
+			validationErrors: errors.array(),
+		});
+	}
 	Product.findById(prodId)
 		.then((product) => {
 			if (product.userId.toString() !== req.user._id.toString()) {
