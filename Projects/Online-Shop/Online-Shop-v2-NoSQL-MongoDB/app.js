292066f320275079d538acb27b1bcaaa5ecaddf4
Leon Sch <leoneli61@gmail.com>
2025-03-31 23:00:05 +0300

    Fixing the error middleware

--- Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/app.js
+++ Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/app.js
@@ -43,6 +43,13 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+	//Make data available to all templates
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
+
+app.use((req, res, next) => {
 	if (!req.session.user) {
 		return next();
 	}
@@ -55,15 +62,8 @@ app.use((req, res, next) => {
 			next();
 		})
 		.catch((err) => {
-			throw new Error(err);
+			next(new Error(err)); // Pass the error to the next middleware
 		});
-});
-
-app.use((req, res, next) => {
-	//Make data available to all templates
-	res.locals.isAuthenticated = req.session.isLoggedIn;
-	res.locals.csrfToken = req.csrfToken();
-	next();
 });
 
 app.use('/admin', adminRoutes);
@@ -75,8 +75,11 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-	// res.status(error.httpStatusCode).render(...);
-	res.redirect('/500');
+	res.status(500).render('500', {
+		pageTitle: 'An error occurred',
+		path: '/500',
+		isAuthenticated: req.session.isLoggedIn,
+	});
 }); //Error handling middleware
 
 mongoose
