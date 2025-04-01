92fdb534c577c3248b50d4c6328fb6a603157540
LeonSch <leoneli61@gmail.com>
2025-04-01 12:13:58 +0300

    Installing mulder, adding it to the form

--- Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/controllers/admin.js
+++ Projects/Online-Shop/Online-Shop-v2-NoSQL-MongoDB/controllers/admin.js
@@ -35,7 +35,7 @@ exports.postAddProduct = (req, res, next) => {
 	const title = req.body.title;
 	const price = req.body.price;
 	const description = req.body.description;
-	const imageUrl = req.body.imageUrl;
+	const imageUrl = req.body.image;
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(422).render('admin/edit-product', {
@@ -121,7 +121,7 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedTitle = req.body.title;
 	const updatedPrice = req.body.price;
 	const updatedDescription = req.body.description;
-	const updatedImageUrl = req.body.imageUrl;
+	const updatedImageUrl = req.body.image;
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(422).render('admin/edit-product', {
