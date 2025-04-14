ac0a6b1e48bf5b269c887a66e77151560df20765
LeonSch <leoneli61@gmail.com>
2025-04-14 14:28:52 +0300

    Adding create post endpoint and server side validation

--- Projects/Blog/Backend/controllers/feed.js
+++ Projects/Blog/Backend/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
 	const posts = [
 		{
@@ -18,10 +20,25 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-	const id = new Date().toISOString();
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			message: 'Validation failed, entered data is incorrect.',
+			errors: errors.array(),
+		});
+	}
+	const _id = new Date().toISOString();
 	const title = req.body.title;
 	const content = req.body.content;
-	const post = { id, title, content };
+	const creator = { name: 'LeonZZ' };
+	const createdAt = new Date();
+	const post = {
+		_id,
+		title,
+		content,
+		creator,
+		createdAt,
+	};
 	res.status(201).json({
 		message: 'Post created successfully!',
 		post: post,
--- Projects/Blog/Backend/routes/feed.js
+++ Projects/Blog/Backend/routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -6,6 +7,19 @@ const router = express.Router();
 
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
-router.post('/post', feedController.createPost);
+router.post(
+	'/post',
+	[
+		body('title')
+			.trim()
+			.isLength({ min: 5 })
+			.withMessage('Title must be at least 5 characters long.'),
+		body('content')
+			.trim()
+			.isLength({ min: 5 })
+			.withMessage('Content must be at least 5 characters long.'),
+	],
+	feedController.createPost
+);
 
 module.exports = router;
