125c2d10cc8bb1675f179dfb49b12d66799427f3
LeonSch <leoneli61@gmail.com>
2025-04-14 16:55:31 +0300

    setting image serving and error handling

--- Projects/Blog/Backend/app.js
+++ Projects/Blog/Backend/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +11,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(__dirname + '/images'));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,6 +24,15 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	res.status(status).json({
+		message: message,
+	});
+});
 
 mongoose
 	.connect(process.env.MONGO_URI)
