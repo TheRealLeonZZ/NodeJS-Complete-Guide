edbd9501620a80d27a5a3c080118fbc4d63e74ad
LeonSch <leoneli61@gmail.com>
2025-04-14 14:38:03 +0300

    setting up post model and connection to mongoose

--- Projects/Blog/Backend/app.js
+++ Projects/Blog/Backend/app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -19,4 +22,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		app.listen(8080);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
