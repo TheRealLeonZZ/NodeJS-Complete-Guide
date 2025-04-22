7e6c203b45abfa503d302671c4d005ee5670edf2
LeonSch <leoneli61@gmail.com>
2025-04-22 13:54:37 +0300

    Added updating posts

--- Projects/Blog/Backend/routes/feed.js
+++ Projects/Blog/Backend/routes/feed.js
@@ -22,5 +22,19 @@ router.post(
 	feedController.createPost
 );
 router.get('/post/:postId', feedController.getPost);
+router.put(
+	'/post/:postId',
+	[
+		body('title')
+			.trim()
+			.isLength({ min: 5 })
+			.withMessage('Title must be at least 5 characters long.'),
+		body('content')
+			.trim()
+			.isLength({ min: 5 })
+			.withMessage('Content must be at least 5 characters long.'),
+	],
+	feedController.updatePost
+);
 
 module.exports = router;
