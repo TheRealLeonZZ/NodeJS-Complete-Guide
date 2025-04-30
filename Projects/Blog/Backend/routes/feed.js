fd6325a2c765fae99cca8c949a7c6375672930ac
LeonSch <leoneli61@gmail.com>
2025-04-30 19:06:07 +0300

    adding auth middleware to all routes & in frontend

--- Projects/Blog/Backend/routes/feed.js
+++ Projects/Blog/Backend/routes/feed.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.get('/posts', isAuth, feedController.getPosts);
 router.post(
 	'/post',
+	isAuth,
 	[
 		body('title')
 			.trim()
@@ -22,9 +23,10 @@ router.post(
 	],
 	feedController.createPost
 );
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 router.put(
 	'/post/:postId',
+	isAuth,
 	[
 		body('title')
 			.trim()
@@ -37,6 +39,6 @@ router.put(
 	],
 	feedController.updatePost
 );
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
