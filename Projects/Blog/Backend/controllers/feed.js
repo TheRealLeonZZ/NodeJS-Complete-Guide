3ab3d15556dd6c7d65642ec0d47b3806e8b87fa7
LeonSch <leoneli61@gmail.com>
2025-05-01 10:09:02 +0300

    editing createPost to async

--- Projects/Blog/Backend/controllers/feed.js
+++ Projects/Blog/Backend/controllers/feed.js
@@ -32,7 +32,7 @@ exports.getPosts = async (req, res, next) => {
 	}
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		const error = new Error('Validation failed, entered data is incorrect.');
@@ -54,29 +54,28 @@ exports.createPost = (req, res, next) => {
 		imageUrl: imageUrl,
 		creator: req.userId,
 	});
-	post
-		.save()
-		.then((result) => {
-			return User.findById(req.userId);
-		})
-		.then((user) => {
-			creator = user;
-			user.posts.push(post);
-			return user.save();
-		})
-		.then((result) => {
-			res.status(201).json({
-				message: 'Post created successfully!',
-				post: post,
-				creator: { _id: creator._id, name: creator.name },
-			});
-		})
-		.catch((err) => {
-			if (!err.statusCode) {
-				err.statusCode = 500;
-			}
-			next(err);
+	try {
+		const user = await User.findById(req.userId);
+		if (!user) {
+			const error = new Error('User not found.');
+			error.statusCode = 404;
+			throw error;
+		}
+		await post.save();
+		const creator = user;
+		user.posts.push(post);
+		await user.save();
+		res.status(201).json({
+			message: 'Post created successfully!',
+			post: post,
+			creator: { _id: creator._id, name: creator.name },
 		});
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
 };
 
 exports.getPost = (req, res, next) => {
