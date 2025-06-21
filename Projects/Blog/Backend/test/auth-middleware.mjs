169b0b454f12d58086aef2a431e59eb723f2b583
LeonSch <leoneli61@gmail.com>
2025-06-21 16:35:59 +0300

    adding more complex tests to auth and mocking functions using sinon

--- Projects/Blog/Backend/test/auth-middleware.mjs
+++ Projects/Blog/Backend/test/auth-middleware.mjs
@@ -1,4 +1,6 @@
 import { expect } from 'chai';
+import jwt from 'jsonwebtoken';
+import sinon from 'sinon'; // Importing sinon for mocking
 
 import authMiddleware from '../middleware/is-auth.js';
 
@@ -21,5 +23,28 @@ describe('Auth Middleware', () => {
 			},
 		};
 		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+	});
+
+	it('should throw an error if token is not verified', () => {
+		const req = {
+			get: function () {
+				return 'Bearer xyz'; // Simulate invalid token
+			},
+		};
+		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+	});
+
+	it('should yield a userId after decoding the token', () => {
+		const req = {
+			get: function () {
+				return 'Bearer asfsafsafsa'; // Simulate invalid Authorization header
+			},
+		};
+		sinon.stub(jwt, 'verify'); // Stub jwt.verify to control its behavior
+		jwt.verify.returns({ userId: '12345' }); // Simulate a decoded token
+		authMiddleware(req, {}, () => {}); // Call the middleware
+		expect(jwt.verify.called).to.be.true; // Check if jwt.verify was called
+		expect(req).to.have.property('userId'); // Check if userId was set
+		jwt.verify.restore(); // Restore the original jwt.verify method
 	});
 });
