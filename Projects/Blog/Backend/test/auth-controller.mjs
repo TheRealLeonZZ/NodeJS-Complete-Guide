279a72e23eafba88ae4cc38f6b5a74868eeb7eb8
LeonSch <leoneli61@gmail.com>
2025-06-26 00:21:15 +0300

    Adding before and after hooks

--- Projects/Blog/Backend/test/auth-controller.mjs
+++ Projects/Blog/Backend/test/auth-controller.mjs
@@ -8,7 +8,25 @@ import mongoose from 'mongoose'; // Importing mongoose for database operations
 import User from '../models/user.js';
 import authController from '../controllers/auth.js';
 
-describe('Auth Controller - Login', () => {
+describe('Auth Controller', () => {
+	before((done) => {
+		mongoose
+			.connect(process.env.MONGO_TEST_URI)
+			.then(() => {
+				const user = new User({
+					email: 'test@test.com',
+					password: 'abc123',
+					name: 'Test User',
+					posts: [],
+					_id: '68223bc816700cd0e2fbb08b',
+				});
+				return user.save(); // Saving the user to the database
+			})
+			.then(() => {
+				done();
+			});
+	});
+
 	it('should throw an error with code 500 if accessing the database fails', (done) => {
 		sinon.stub(User, 'findOne'); // Stubbing User.findOne method
 		User.findOne.throws(); // Simulating a database access failure
@@ -32,50 +50,36 @@ describe('Auth Controller - Login', () => {
 	});
 
 	it('should send a response with a valid user status for an existing user', (done) => {
-		mongoose
-			.connect(process.env.MONGO_TEST_URI)
-			.then(() => {
-				const user = new User({
-					email: 'test@test.com',
-					password: 'abc123',
-					name: 'Test User',
-					posts: [],
-					_id: '68223bc816700cd0e2fbb08b',
-				});
-				return user.save(); // Saving the user to the database
-			})
-			.then(() => {
-				const req = {
-					userId: '68223bc816700cd0e2fbb08b',
-				};
-				const res = {
-					statusCode: 500,
-					userStatus: null,
-					status(code) {
-						this.statusCode = code; // Setting the status code
-						return this; // Returning the response object for chaining
-					},
-					json(data) {
-						this.userStatus = data.status; // Setting the user status
-					},
-				};
-				authController
-					.getStatus(req, res, () => {})
-					.then((result) => {
-						console.log(result);
-						expect(res.statusCode).to.equal(200); // Expecting status code 200
-						expect(res.userStatus).to.equal('I am new!'); // Expecting user status to be 'I am new!'
-						User.deleteMany({})
-							.then(() => {
-								return mongoose.disconnect();
-							})
-							.then(() => {
-								done(); // Indicating that the test is done
-							}); // Cleaning up the database after the test
-					});
-			})
-			.catch((err) => {
-				console.log(err);
+		const req = {
+			userId: '68223bc816700cd0e2fbb08b',
+		};
+		const res = {
+			statusCode: 500,
+			userStatus: null,
+			status(code) {
+				this.statusCode = code; // Setting the status code
+				return this; // Returning the response object for chaining
+			},
+			json(data) {
+				this.userStatus = data.status; // Setting the user status
+			},
+		};
+		authController
+			.getStatus(req, res, () => {})
+			.then((result) => {
+				console.log(result);
+				expect(res.statusCode).to.equal(200); // Expecting status code 200
+				expect(res.userStatus).to.equal('I am new!'); // Expecting user status to be 'I am new!'
+				done(); // Indicating that the test is done
 			});
+	});
+	after((done) => {
+		User.deleteMany({})
+			.then(() => {
+				return mongoose.disconnect();
+			})
+			.then(() => {
+				done(); // Indicating that the test is done
+			}); // Cleaning up the database after the test
 	});
 });
