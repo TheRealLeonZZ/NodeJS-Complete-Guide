26b4b820f257186d6d172a87094b31c73afdd28b
LeonSch <leoneli61@gmail.com>
2025-07-02 16:02:53 +0300

    Working with arrays and objects

--- Exercises/TypeScript-Practice/app.js
+++ Exercises/TypeScript-Practice/app.js
@@ -2,6 +2,8 @@
 const num1Input = document.getElementById('num1');
 const num2Input = document.getElementById('num2');
 const button = document.querySelector('button');
+const numResults = [];
+const textResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -11,11 +13,17 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+    console.log(resultObj.timestamp);
+}
 button.addEventListener('click', () => {
     const num1 = num1Input.value;
     const num2 = num2Input.value;
     const result = add(+num1, +num2);
+    numResults.push(result);
     const stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
--- Exercises/TypeScript-Practice/app.ts
+++ Exercises/TypeScript-Practice/app.ts
@@ -2,6 +2,9 @@ const num1Input = document.getElementById('num1') as HTMLInputElement;
 const num2Input = document.getElementById('num2') as HTMLInputElement;
 const button = document.querySelector('button')!;
 
+const numResults: number[] = [];
+const textResults: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
 	if (typeof num1 === 'number' && typeof num2 === 'number') {
 		return num1 + num2;
@@ -11,11 +14,18 @@ function add(num1: number | string, num2: number | string) {
 	return +num1 + +num2;
 }
 
+function printResult(resultObj: { val: number | string; timestamp: Date }) {
+	console.log(resultObj.val);
+	console.log(resultObj.timestamp);
+}
+
 button.addEventListener('click', () => {
 	const num1 = num1Input.value;
 	const num2 = num2Input.value;
 	const result = add(+num1, +num2);
+	numResults.push(result as number);
 	const stringResult = add(num1, num2);
-	console.log(result);
-	console.log(stringResult);
+	textResults.push(stringResult as string);
+	printResult({ val: result as number, timestamp: new Date() });
+	console.log(numResults, textResults);
 });
