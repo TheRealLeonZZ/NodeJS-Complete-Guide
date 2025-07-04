0b5bd1a0b5073cfcdf495a292c8f7f09ed91c639
Leon Sch <leoneli61@gmail.com>
2025-07-05 02:12:53 +0300

    finished routes for todos

--- Exercises/Basic-Node-Ts-Practice/routes/todos.js
+++ Exercises/Basic-Node-Ts-Practice/routes/todos.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
-const todos = [];
+let todos = [];
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
@@ -13,5 +13,22 @@ router.post('/todo', (req, res, next) => {
     };
     todos.push(newTodo);
     res.status(201).json({ todo: newTodo });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todo) => todo.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex].text = req.body.text;
+        res.status(200).json({ todo: todos[todoIndex] });
+    }
+    else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter((todoItem) => {
+        todoItem.id !== req.params.todoId;
+    });
+    res.status(200).json({ message: 'Todo deleted' });
 });
 exports.default = router;
--- Exercises/Basic-Node-Ts-Practice/routes/todos.ts
+++ Exercises/Basic-Node-Ts-Practice/routes/todos.ts
@@ -4,7 +4,7 @@ import { Todo } from '../models/todo.js';
 
 const router = Router();
 
-const todos: Array<Todo> = [];
+let todos: Array<Todo> = [];
 
 router.get('/', (req, res, next) => {
 	res.status(200).json({ todos: todos });
@@ -17,6 +17,24 @@ router.post('/todo', (req, res, next) => {
 	};
 	todos.push(newTodo);
 	res.status(201).json({ todo: newTodo });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+	const tid = req.params.todoId;
+	const todoIndex = todos.findIndex((todo) => todo.id === tid);
+	if (todoIndex >= 0) {
+		todos[todoIndex].text = req.body.text;
+		res.status(200).json({ todo: todos[todoIndex] });
+	} else {
+		res.status(404).json({ message: 'Todo not found' });
+	}
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+	todos = todos.filter((todoItem) => {
+		todoItem.id !== req.params.todoId;
+	});
+	res.status(200).json({ message: 'Todo deleted' });
 });
 
 export default router;
