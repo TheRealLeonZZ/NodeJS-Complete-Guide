d4d9c77b2ac21282885392e5400086372694011e
Leon Sch <leoneli61@gmail.com>
2025-07-05 02:21:46 +0300

    Using type casting for params and req body

--- Exercises/Basic-Node-Ts-Practice/routes/todos.js
+++ Exercises/Basic-Node-Ts-Practice/routes/todos.js
@@ -7,18 +7,21 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: Math.random().toString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ todo: newTodo });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const body = req.body;
+    const params = req.params;
+    const tid = params.todoId;
     const todoIndex = todos.findIndex((todo) => todo.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex].text = req.body.text;
+        todos[todoIndex].text = body.text;
         res.status(200).json({ todo: todos[todoIndex] });
     }
     else {
@@ -26,8 +29,9 @@ router.put('/todo/:todoId', (req, res, next) => {
     }
 });
 router.delete('/todo/:todoId', (req, res, next) => {
+    const params = req.params;
     todos = todos.filter((todoItem) => {
-        todoItem.id !== req.params.todoId;
+        return todoItem.id !== params.todoId;
     });
     res.status(200).json({ message: 'Todo deleted' });
 });
--- Exercises/Basic-Node-Ts-Practice/routes/todos.ts
+++ Exercises/Basic-Node-Ts-Practice/routes/todos.ts
@@ -6,24 +6,35 @@ const router = Router();
 
 let todos: Array<Todo> = [];
 
+type RequestBody = {
+	text: string;
+};
+
+type RequestParams = {
+	todoId: string;
+};
+
 router.get('/', (req, res, next) => {
 	res.status(200).json({ todos: todos });
 });
 
 router.post('/todo', (req, res, next) => {
+	const body: RequestBody = req.body;
 	const newTodo: Todo = {
 		id: Math.random().toString(),
-		text: req.body.text,
+		text: body.text,
 	};
 	todos.push(newTodo);
 	res.status(201).json({ todo: newTodo });
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-	const tid = req.params.todoId;
+	const body: RequestBody = req.body;
+	const params: RequestParams = req.params;
+	const tid = params.todoId;
 	const todoIndex = todos.findIndex((todo) => todo.id === tid);
 	if (todoIndex >= 0) {
-		todos[todoIndex].text = req.body.text;
+		todos[todoIndex].text = body.text;
 		res.status(200).json({ todo: todos[todoIndex] });
 	} else {
 		res.status(404).json({ message: 'Todo not found' });
@@ -31,8 +42,9 @@ router.put('/todo/:todoId', (req, res, next) => {
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
+	const params: RequestParams = req.params;
 	todos = todos.filter((todoItem) => {
-		todoItem.id !== req.params.todoId;
+		return todoItem.id !== params.todoId;
 	});
 	res.status(200).json({ message: 'Todo deleted' });
 });
